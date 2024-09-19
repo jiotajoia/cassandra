76c5ec057b92fe563300fef0e96578bc9df495c8
Jio <bleibeducuz@gmail.com>
2024-09-18 21:06:58 -0300

    alteração nos códigos do card

--- basic/cards.sql
+++ basic/cards.sql
@@ -1,29 +1,40 @@
 --keyspace
 
-CREATE KEYSPACE meu_novo_keyspace WITH replication {'class':'SimpleStrategy', 'replication_factor': 1};
+CREATE KEYSPACE loja WITH replication = {'class':'SimpleStrategy', 'replication_factor': 1};
 
 --para usar/criar o keyspace
 
-USE meu_novo_keyspace;
+USE loja;
 
 --criando a tabela
 
-CREATE TABLE produto {
+CREATE TABLE produto (
     id UUID PRIMARY KEY,
     nome text
-};
+);
+
+CREATE TABLE funcionario (
+    id UUID PRIMARY KEY,
+    nome text,
+    idade int
+);
 
 --insert na tabela produto
 
-INSERT INTO produto 
+INSERT INTO produto (id, nome)
 VALUES(uuid(), 'caderno');
 
+INSERT INTO funcionario (id, nome, idade)
+VALUES(uuid(), 'jiotajoia', 21);
 --select
 
 SELECT id FROM produto
-WHERE nome = 'caderno';
+WHERE nome = 'caderno' ALLOW FILTERING;
 
 --delete
 
 DELETE FROM produto
-WHERE nome = 'caderno';
+WHERE id = ;
+
+--drop
+DROP KEYSPACE loja;
